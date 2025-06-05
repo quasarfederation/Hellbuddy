5df48ca90f8256ac51de60f6ec5f9851e2473592
quasarfederation <rr>
2025-06-04 20:53:55 -0400

    Created repo

---
+++ helldiversapi.py
@@ -0,0 +1,76 @@
+import requests
+
+"""HelldiversAPI is a wrapper that handles information from the Training Manual API. Ideally it should abstract away anything related to the api, and return the desired info."""
+class HelldiversAPI:
+    api_link = "https://helldiverstrainingmanual.com/api/v1/"
+
+    @classmethod
+    def get_dispatch(cls, time=41000001):
+        dispatch_params = {"from": time}
+        response = requests.get(f"{cls.api_link}war/news", params=dispatch_params)
+        dispatch = []
+
+        if response.ok:
+            news_feed = response.json()
+            dispatch = map(lambda l: HelldiversAPI._clean_json("\n", l, "message"), news_feed)
+        else:
+            print(f"Could not retrieve data. Error Code: {response.status_code}")
+
+        return list(dispatch)
+
+    @classmethod
+    def get_major_order(cls):
+        response = requests.get(f"{cls.api_link}war/major-orders")
+        major_order_data = []
+
+        if response.ok:
+            major_order = (response.json()[0]["setting"])
+            major_order_data.append(major_order["overrideTitle"])
+            major_order_data.append(major_order["overrideBrief"])
+            major_order_data.append(major_order["taskDescription"])
+        else:
+            print(f"Could not retrieve data. Error Code: {response.status_code}")
+
+        return major_order_data
+
+    @classmethod
+    def get_campaign_info(cls):
+        response = requests.get(f"{cls.api_link}war/campaign")
+        planet_data = []
+
+        if response.ok:
+            for planet in response.json():
+                planet_data.append(planet)
+        else:
+            print(f"Could not retrieve data. Error Code: {response.status_code}")
+
+        return planet_data
+
+    @classmethod
+    def get_planet_info(cls, planet_name):
+        response = requests.get(f"{cls.api_link}planets")
+        planet_info = []
+
+        if response.ok:
+            planet_list = response.json()
+            for planet in planet_list:
+                if planet_list[planet]["name"] == planet_name:
+                    planet_info = planet_list[planet]
+                    break
+        else:
+            print(f"Could not retrieve data. Error Code: {response.status_code}")
+
+        if not planet_info:
+            print(f"Could not find planet from name: {planet_name}")
+            return planet_info
+        else:
+            return planet_info
+
+    # Probably a better way to do this, but this should work reliably whereas just using .find() wouldn't work.
+    @staticmethod
+    def _clean_json(test_char, json, category):
+        start_index = 0
+        for index, char in enumerate(json[category]):
+            if char == test_char:
+                start_index = index
+        return json[category][start_index:len(json[category])]
---
+++ helldiversgui.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+import pyglet
+from helldiversapi import *
+pyglet.options["win32_gdi_font"] = True
+
+window = tk.Tk()
+pyglet.font.add_file("assets\\FS Sinclair Regular.otf")
+pyglet.font.add_file("assets\\FS Sinclair Bold.otf")
+label = tk.Label(window, text="Coretta Kelly Newsfeed", fg="yellow", bg="gray6", font=("FS Sinclair", 27, "bold"), pady=25)
+text_output = tk.Text(window, bg="gray10", fg="yellow", font=("FS Sinclair", 15), height=17)
+dispatch_btn = tk.Button(window, text="Get dispatch", font=("FS Sinclair", 16), bg="gray10", fg="yellow", pady=25, padx=25, overrelief="sunken")
+major_order_btn = tk.Button(window, text="Get MO", font=("FS Sinclair", 16), bg="gray10", fg="yellow", pady=25, padx=25, overrelief="sunken")
+
+def main():
+    window.config(bg="gray6")
+    window.resizable(False, False)
+    window.geometry("1280x720")
+    window.iconbitmap("assets\\icon.ico")
+    window.title("Hellbuddy")
+
+    text_output.config(state="disabled")
+
+    dispatch_btn.config(command=lambda: on_button_click("dispatch"))
+    major_order_btn.config(command=lambda: on_button_click("major order"))
+
+    label.pack(fill="x")
+    text_output.pack(side="right", expand=True)
+    dispatch_btn.pack(side="left", expand=True)
+    major_order_btn.pack(side="left", expand=True)
+
+    window.mainloop()
+
+def on_button_click(identifier):
+    match identifier:
+        case "dispatch":
+            text_output.config(state="normal")
+            text_output.delete("0.0", "end")
+            text_output.insert("end", "Connecting to Station 5...\n")
+            for message in HelldiversAPI.get_dispatch():
+                text_output.insert("end", "BROADCAST:\n" + message + "\n")
+            text_output.config(state="disabled")
+        case "major order":
+            text_output.config(state="normal")
+            text_output.delete("0.0", "end")
+            text_output.insert("0.0", "Connecting to Station 16...\nBROADCAST:\n")
+            for message in HelldiversAPI.get_major_order():
+                text_output.insert("end", message + "\n")
+            text_output.config(state="disabled")
+
+if __name__ == "__main__":
+    main()
